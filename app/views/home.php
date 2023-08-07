59681be630e01981d22e9ec1a8cdc64e94e1bbf3
raviel96 <manuelfontaine971@gmail.com>
2023-08-07 12:39:20 -0400

    Création de la section : notre expertise

--- app/views/home.php
+++ app/views/home.php
@@ -16,7 +16,7 @@ $this->title = "Accueil";
         <h2>L'ACCOMPAGNEMENT <span class="text-highlight">SUR MESURE</span> !</h2>
     </div>
     <section class="section-vision">
-        <h2 class="section-title">NOTRE <span class="text-highlight">VISION</span></h2>
+        <h2 class="section-title">Notre <span class="text-highlight">vision</span></h2>
         <div class="section-container">
             <div class="section-image">
                 <img src="" alt="">
@@ -25,6 +25,31 @@ $this->title = "Accueil";
                 <p>L’association a été créée en 2008. En 2009, l’opportunité d’accompagner les bénéficiaires de contrats à durée déterminée d’insertion, salariés d’une structure d’insertion par l’activité économique (SIAE) est arrivée.</p>
                 <p>Depuis, nous avons fait de l’accompagnement social et professionnel notre activité principale.
                 Notre vision : <strong>Accompagner</strong>, <strong>Challenger</strong>, <strong>Propulser</strong>, <strong>Insérer</strong> !</p>
+            </div>
+        </div>
+    </section>
+    <section class="section-expert">
+        <h2 class="section-title">Notre <span class="text-highlight">expertise</span></h2>
+        <div class="section-container">
+            <div class="section-image">
+                <img src="" alt="">
+            </div>
+            <div class="section-content">
+                <p>L’intervention d’APSA CARE auprès de ses bénéficiaires, se situe au niveau de l’apport d’informations préventives et de la mise en relation avec tous partenaires pouvant aider à la remédiation des problématiques détectées par notre équipe d’experts :</p>
+                <ul>
+                    <li>
+                        <p>Une ancienne formatrice AFPA, actrice de la formation professionnelle d’adultes depuis 1992.</p>
+                    </li>
+                    <li>
+                        <p>Une assistante sociale, intégrée dans l’équipe d’accompagnement.</p>
+                    </li>
+                    <li>
+                        <p>2 psychologues cliniciennes qui détectent d’éventuelles problématiques psychiques et cognitives.</p>
+                    </li>
+                    <li>
+                        <p>Enfin, nous mobilisons notre réseau pour répondre aux besoins de nos bénéficiaires.</p>
+                    </li>
+                </ul>
             </div>
         </div>
     </section>
